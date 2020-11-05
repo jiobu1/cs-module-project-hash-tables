08d3076abe604a4e6fd4aceb9e782106a91d2ce4
Jisha Obukwelu <jisha@obukwelu.com>
2020-11-04 21:25:07 -0500

    Expensive Seq_Cost decreased

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,8 +1,17 @@
 # Your code here
 
+table = {}
+
 
 def expensive_seq(x, y, z):
-    # Your code here
+
+    # key = f'{x},{y},{z}'
+    if x <= 0:
+       return y + z
+    if (x,y,z) not in table:
+        table[(x,y,z)] = expensive_seq(x - 1, y + 1, z) + expensive_seq(x - 2, y + 2, z * 2) + expensive_seq(x - 3, y + 3, z * 3)
+
+    return table[(x,y,z)]
 
 
 
@@ -12,3 +21,10 @@ if __name__ == "__main__":
         print(f"{i*2} {i*3} {i*4} = {x}")
 
     print(expensive_seq(150, 400, 800))
+
+"""
+----------------------------------------------------------------------
+Ran 1 test in 0.225s
+
+OK
+"""
