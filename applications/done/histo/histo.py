e25d3cb83fcf2a0571df896079b5f78afb339a0c
Jisha Obukwelu <jisha@obukwelu.com>
2020-11-05 21:36:01 -0500

    Move completed assignment to done folder

---
+++ applications/done/histo/histo.py
@@ -0,0 +1,31 @@
+import re
+import matplotlib.pyplot as plt
+
+file = open("cs-module-project-hash-tables/applications/done/histo/robin.txt", "r+")
+text = file.read()
+
+
+
+text = text.lower()
+words = re.split(r"[^\w']+",text)
+
+print(f"Number of words in text file:  {len(words)}")
+
+for word in words:
+    if word in d:
+        d[word] +=1
+    else:
+        d[word] = 1
+
+print(d)
+
+len(d.keys())
+
+d = {k: v for k, v in sorted(d.items(), key=lambda item: item[1], reverse=True)}
+
+plt.figure(figsize=(20,70))
+plt.barh(list(d.keys()), d.values(), height=0.5, color='g')
+plt.ylim(-1, 411)
+plt.show()
+
+
