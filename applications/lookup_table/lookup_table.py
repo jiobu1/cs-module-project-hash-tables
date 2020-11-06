65aeb05cbe9cdabb5c0f96e4d16cd3630bb916d2
Jisha Obukwelu <jisha@obukwelu.com>
2020-11-05 21:32:18 -0500

    Lookup_table done

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,6 @@
 # Your code here
+import random
+import math
 
 
 def slowfun_too_slow(x, y):
@@ -9,13 +11,23 @@ def slowfun_too_slow(x, y):
 
     return v
 
+table = {}
+
+def build_look_up_table(x,y):
+    for x in range(2,14):
+        for y in range(3,6):
+            table[(x,y)] = slowfun_too_slow(x,y)
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+    if (x,y) not in table:
+        table[(x,y)] = slowfun_too_slow(x,y)
 
+    return table[(x,y)]
 
 
 # Do not modify below this line!
