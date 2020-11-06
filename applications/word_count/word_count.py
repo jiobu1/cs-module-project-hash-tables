b67510d9061d002a5aa59acfdf3f40d4504324aa
Jisha Obukwelu <jisha@obukwelu.com>
2020-11-05 20:43:00 -0500

    Complete

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,48 @@
+import re
+
 def word_count(s):
     # Your code here
+    s = s.lower()
+
+    # words = re.split(r"[^\w']+",text)
+    # tried this but it did not work
+    no_count = [
+                '"',
+                ":",
+                ";",
+                ",",
+                ".",
+                "-",
+                "+",
+                "=",
+                "/",
+                "\\",
+                "|",
+                "[",
+                "]",
+                "{",
+                "}",
+                "(",
+                ")",
+                "*",
+                "^",
+                "&",
+    ]
+
+    for char in no_count:
+        s = s.replace(char, "")
+
+    d = {}
+
+    words = s.split()
+
+    for word in words:
+        if word in d:
+            d[word] +=1
+        else:
+            d[word] = 1
+    return d
+
 
 
 
