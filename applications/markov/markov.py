79134f097911640c007474411e6573964be1c423
Jisha Obukwelu <jisha@obukwelu.com>
2020-11-10 09:12:45 -0500

    Markov

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -25,22 +25,8 @@ for index, word in enumerate(words):
     if word[-1] in ".?1":
         stop_words.append(word)
 
-    if index <len(words)-1:
-        phrases[word].append(words[index+1])
 
 # print(start_words)
 # print(stop_words)
 # print(phrases)
 
-for i in range(5):
-    word = random.choice(start_words)
-
-    if word not in stop_words:
-        next_word = random.choice(phrases[word])
-
-        while next_word not in stop_words:
-            word += " " + next_word
-            next_word = random.choice(phrases[next_word])
-        word +=  " " + next_word
-
-print(word + '\n')
